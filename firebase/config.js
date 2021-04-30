40c93c8600ed25b79c78d2b488f9008457ded399
Anuragns26 <74043485+Anuragns26@users.noreply.github.com>
2021-04-30 13:03:26 +0530

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -3,13 +3,13 @@ import "@firebase/auth";
 import "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBlWrj_RRWRw--473iHfn90BwIv1_CJtjI",
-  authDomain: "paint-shop-d75b2.firebaseapp.com",
-  projectId: "paint-shop-d75b2",
-  storageBucket: "paint-shop-d75b2.appspot.com",
-  messagingSenderId: "478737577706",
-  appId: "1:478737577706:web:1389d6dd6ad58f481a4993",
-  measurementId: "G-WBQDE1ZEG1",
+  apiKey: "********************",
+  authDomain: "********************.firebaseapp.com",
+  projectId: "********************",
+  storageBucket: "********************.appspot.com",
+  messagingSenderId: "********************",
+  appId: "********************",
+  measurementId: "********************",
 };
 
 if (firebase.apps.length === 0) {
